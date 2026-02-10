f6772a5caa4b1c17a17bb564f51412cbf538ed74
Roman Acevedo <roman.acevedo62@gmail.com>
2026-02-10 12:38:26 +0100

    test(nsfiles): test

--- e2e_tests/nsfiles_usecases_test.go
+++ e2e_tests/nsfiles_usecases_test.go
@@ -1,6 +1,8 @@
 package e2e_tests
 
 import (
+	"crypto/rand"
+	"encoding/hex"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -20,8 +22,40 @@ func TestNsfilesUpload_namespaceNotFound(t *testing.T) {
 }
 
 func TestNsfilesUpload_allowRelative(t *testing.T) {
-	_, stderr, err := RunAuthenticatedCliCmd(t, "nsfiles", "upload", "company", "./", "--path", "./")
-	// TODO
+	t.Skip("to handle") // TODO
+	_, stderr, err := RunAuthenticatedCliCmd(t, "nsfiles", "upload", "system", "./", "--path", "./", "--override")
 	require.Empty(t, stderr)
 	require.NoError(t, err)
+}
+
+func TestNsfilesUpload_e2e(t *testing.T) {
+	fileNsPath := getRandomNsFilePath()
+	_, _, err := RunAuthenticatedCliCmd(t, "nsfiles", "get", "system", "--path", fileNsPath)
+	require.Error(t, err, "file should not exist before test")
+
+	_, _, err = RunAuthenticatedCliCmd(t, "nsfiles", "upload", "system", "./README.MD", "--path", fileNsPath)
+	require.NoError(t, err)
+
+	stdout, stderr, err := RunAuthenticatedCliCmd(t, "nsfiles", "get", "system", "--path", fileNsPath)
+	require.Empty(t, stderr)
+	require.NoError(t, err, "file should exist after upload")
+	require.Contains(t, stdout, "e2e tests")
+
+	_, _, err = RunAuthenticatedCliCmd(t, "nsfiles", "delete", "system", "--path", fileNsPath)
+	require.NoError(t, err)
+
+	_, _, err = RunAuthenticatedCliCmd(t, "nsfiles", "get", "system", "--path", fileNsPath)
+	require.Error(t, err, "file should have been deleted")
+}
+
+func getRandomNsFilePath() string {
+	return "e2e-nsfiles-tests/" + randomId()
+}
+func randomId() string {
+	b := make([]byte, 16)
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
+	return hex.EncodeToString(b)
 }
