36697eb5136b14accbdca275d4c6182a24290cd8
Florian Hussonnois <fhussonnois@kestra.io>
2026-03-19 17:18:57 +0100

    feat: add workers registration-tokens generate command
    Add CLI command to generate worker registration tokens locally,
    matching the Java TokenGenerator format (kwreg prefix, base32 payload,
    CRC32 checksum).

--- src/cli/root.go
+++ src/cli/root.go
@@ -93,6 +93,7 @@ with support for multiple authentication contexts and output formats.`,
 	root.AddCommand(newNamespaceFilesCommand())
 	root.AddCommand(newKVCommand())
 	root.AddCommand(newExecutionsCommand())
+	root.AddCommand(newWorkersCommand())
 
 	return root
 }
